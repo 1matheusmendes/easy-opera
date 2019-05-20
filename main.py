d8eb03188a52528967285d39d2414f5705fae89d
1matheusmendes <matheus_mendess@live.com>
2019-05-20 13:02:23 -0300

    indentationError

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ gpio.setmode(gpio.BOARD)
 gpio.setup(11, gpio.OUT)
 
 while True:
-gpio.output(11, gpio.HIGH)
-time.sleep(2)
-gpio.outout(11, gpio.LOW)
-time.sleep(2)
+    gpio.output(11, gpio.HIGH)
+    time.sleep(2)
+    gpio.outout(11, gpio.LOW)
+    time.sleep(2)
 
 gpio.cleanup()
