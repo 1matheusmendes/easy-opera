c72c3412d895a98cb5a09236bf6a724b21b97d5a
Matheus Mendes <matheus_mendess@live.com>
2019-05-20 14:24:21 -0300

    config board para o pino 11 gpio 17

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import RPi.GPIO as gpio
 import time
-
+#configurando como BOARD, identificacao fisica dos pino
 gpio.setmode(gpio.BOARD)
-
+#configurando a direcao dos pino
 gpio.setup(11, gpio.OUT)
+print "Setando modo output no pino11 GPIO 17 [LED VERDE]"
 
-while True:
-    gpio.output(11, gpio.HIGH)
-    time.sleep(2)
-    gpio.output(11, gpio.LOW)
-    time.sleep(2)
+gpio.output(11, gpio.HIGH)
+print "Led verde acesso"
+time.sleep(2)
 
+gpio.output(11, gpio.LOW)
+print "led verde apagado"
+time.sleep(2)
+
+print
+
+#limpando o gpio
 gpio.cleanup()
